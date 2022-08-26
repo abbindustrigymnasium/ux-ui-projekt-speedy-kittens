98de642d6136fef9ad332fef4095695b412dae10
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2022-08-26 15:20:18 +0200

    Del av mitt blogg project som template (I Nuxt)

---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    
+    <div style="width:80vw; margin: 0% 10% 0% 10%;">
+      <div class="quick-about">
+        <div class="flex flex-wrap">
+          <div class="w-full md:w-1/2">
+            <img src="/pictures/Capture.png" width="80%" style="margin:17.5%">
+            <!-- <img src="https://assets-global.website-files.com/5d6ebfe6117b67440d16086f/61261fe24a57aa3847af57ce_fibra%20Copia%20de%20Ima%CC%81genes%20TechBlog.png" width="65%" style="margin:17.5%">
+            <div id="orange-thing"></div> -->
+          </div>
+          <div class="w-full md:w-1/2">
+             <nuxt-content :document="about" style="margin:10%;" class="font-body"></nuxt-content>
+          </div>
+        </div>
+        <h1>Latest posts</h1>
+        <div class="flex flex-wrap" style="justify-content: center; margin-bottom:200px">
+          <div @click="goToDish(page.id)" class="w-56 m-6" style="cursor: pointer;" v-for="page in pages" :key="page.id">
+            <PostItemComponent :item="page"></PostItemComponent>
+          </div>
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({$content}){
+      // let about = await $content('index/short-about').fetch()
+      // let pages = await $content('postsmd').sortBy('nr', 'desc').without(['body']).fetch()
+      // sortBy('id', 'desc').limit() 
+      return {
+          // about, pages
+      }
+  }, 
+  methods: {
+    goToDish(id) {
+      //this.$router.push('/blogpost/'+id)
+    }
+  }
+}
+</script>
+
+
+
+
+<style>
+  .quick-about{
+    width:100%;
+  }
+  #orange-thing{
+    position: absolute; 
+    width:20%; 
+    height:70%; 
+    background-color: #f0b541;
+    top: 67.5%;
+    left: 12%;
+    z-index: -1;
+  }
+  .bloglist{
+    width: 30%;
+  }
+</style>
+
+
+
+// https://www.behance.net/gallery/117672893/Cooking-blog?tracking_source=search_projects%7Cblog%20design
