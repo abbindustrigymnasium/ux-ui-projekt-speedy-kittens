f6e31d343b769aa8d79e7f522f2ae4da64bc8f02
abbnoevah <92145566+abbnoevah@users.noreply.github.com>
2022-09-07 14:03:27 +0200

    gjorde lite saker typ

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div class="pt-24 bg-gray-800">
     
     <div style="width:80vw; margin: 0% 10% 0% 10%;">
       <div class="quick-about">
         <div class="flex flex-wrap">
           <div class="w-full md:w-1/2">
+          <!-- <br>
           <br>
-          <br>
-          <br>
+          <br> -->
             <!-- <img src="/pictures/Capture.png" width="80%" style="margin:17.5%"> -->
             <!-- <img src="https://assets-global.website-files.com/5d6ebfe6117b67440d16086f/61261fe24a57aa3847af57ce_fibra%20Copia%20de%20Ima%CC%81genes%20TechBlog.png" width="65%" style="margin:17.5%">
             <div id="orange-thing"></div> -->
@@ -16,13 +16,20 @@
              <!-- <nuxt-content :document="about" style="margin:10%;" class="font-body"></nuxt-content> -->
           </div>
         </div>
-        <h1>Recomended Books</h1>
-        <div class="flex flex-wrap" style="justify-content: center; margin-bottom:200px">
-          <div @click="goToBook(book.ISBN)" class="w-56 m-6" style="cursor: pointer; background-color: rgb(150, 150, 150);" v-for="book in books" :key="book.id">
-
-              <p>{{book.ISBN}}</p>
-              <p>{{book.title}}</p>
-              <img :src="book.img" alt="">
+        <h1 class="text-gray-300">Recomended Books</h1>
+        <div class="flex flex-wrap bg-gray-700 py-12" style="justify-content: center; box-shadow: 0 0 48px 0 rgba(0,10,20,0.2);">
+          <div @click="goToBook(book.ISBN)" class="h-80 w-60 m-6 bg-gray-600 bg-cover text-gray-200" style="cursor: pointer; box-shadow: 0 12px 24px 0 rgba(0,10,20,0.4);" v-for="book in books" :key="book.id" :style="{
+          backgroundImage: `url(${book.img})`,
+          textShadow: '0 0 6px rgba(0, 0, 30, 1)'
+          }">
+              <!-- <img :src="book.img" alt="" class="w-full h-60 bg-cover">   -->
+              <div class="w-full h-56"></div>
+              <div class="bg-gray-500 bg-opacity-100 h-24 p-4">
+                <!-- <p>{{book.ISBN}}</p> -->
+                <p>{{book.title}}</p>
+                <!-- <p>{{book.author}}</p> -->
+              </div>
+              
           </div>
         </div>
       </div>
@@ -76,6 +83,26 @@ export default {
 
 
 <style>
+
+  * {
+    font-family:
+            'Source Sans Pro',
+            -apple-system,
+            BlinkMacSystemFont,
+            'Segoe UI',
+            Roboto,
+            'Helvetica Neue',
+            Arial,
+            sans-serif;
+        font-size: 16px;
+        word-spacing: 1px;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        box-sizing: border-box;
+  }
+
   .quick-about{
     width:100%;
   }
