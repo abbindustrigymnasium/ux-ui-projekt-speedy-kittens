1c25f87e8e9a3b091d79641725fa9ff9a95f72ce
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2022-09-07 11:04:14 +0200

    Españoles, que pasa mis amigos? Kan klicka på bok

---
+++ pages/book/_id.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- <div style="margin-left:10%; margin-right:10%;">
+        <nuxt-content :document="post"></nuxt-content>
+        <br>
+        <h5>Author: {{ post.author}}</h5>
+        <p>Created: {{ post.created}}</p> 
+        <p>Updated: {{ post.updated}}</p>
+        <br>
+    </div> -->
+    <div>
+        {{book.title}}
+        <img :src="book.img" alt="">
+        {{book.author}}
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({$content, params}){
+        // let post = await $content('postsmd/'+params.id).fetch();
+        let database = require('@/db_books.json');
+        let book
+        for(let i = 0; i<database["books"].length; i++) if(params.id == database["books"][i]["ISBN"]) book = database["books"][i]
+        return {
+            book
+        }
+
+    }
+}
+
+</script>
+
+
+
+
