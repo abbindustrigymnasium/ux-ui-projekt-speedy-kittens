a9a6e2dcdfd539bd3e204d992d21a1881af25234
abbviksko <viktor.skold@abbgymnasiet.segit config --global user.email  viktor.skold@abbgymnasiet.se>
2022-09-13 14:04:49 +0200

    staffchanges

--- pages/staff.vue
+++ pages/staff.vue
@@ -5,14 +5,14 @@
       <div class="h-60 w-80 m-6 p-6 bg-gray-600 bg-cover text-gray-200 font-medium" style="box-shadow: 0 12px 24px 0 rgba(0,10,20,0.3);" v-for="person in people" :key="person.id" :style="{
       textShadow: '0 0 6px rgba(0, 0, 30, 1)'
       }">
-          <img :src="person.avatar" alt="" class="w-20 h-20" style="border-radius: 50%;">  
-          
+          <img :src="person.avatar" alt="" class="w-20 h-20 object-cover" style="border-radius: 50%;">
+
           <p>{{person.name}}</p>
           <p>{{person.role}}</p>
           <p>{{person.email}}</p>
           <p>{{person.phone}}</p>
           <p>{{person.dateOfBirth}}</p>
-          
+
       </div>
     </div>
   </div>
@@ -25,6 +25,6 @@ export default {
       return {
           people
       }
-  }, 
+  },
 }
 </script>
