3ed18255d0e8e2b3175e9cafc1256f46e003b51f
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2022-09-13 13:11:45 +0200

    staff 2

--- pages/staff.vue
+++ pages/staff.vue
@@ -2,14 +2,16 @@
   <div>
     <h1 class="text-gray-300">Our staff</h1>
     <div class="flex flex-wrap bg-gray-700 py-12" style="justify-content: center; box-shadow: 0 0 48px 0 rgba(0,10,20,0.3);">
-      <div class="h-80 w-60 m-6 bg-gray-600 bg-cover text-gray-200 font-medium" style="cursor: pointer; box-shadow: 0 12px 24px 0 rgba(0,10,20,0.3);" v-for="person in people" :key="person.id" :style="{
+      <div class="h-60 w-80 m-6 p-6 bg-gray-600 bg-cover text-gray-200 font-medium" style="box-shadow: 0 12px 24px 0 rgba(0,10,20,0.3);" v-for="person in people" :key="person.id" :style="{
       textShadow: '0 0 6px rgba(0, 0, 30, 1)'
       }">
-          <img :src="person.avatar" alt="" class="w-full h-60 bg-cover">  
-          <div class="w-full h-56"></div>
-          <div class="bg-gray-500 bg-opacity-100 h-24 p-4">
-            <p>{{person.name}}</p>
-          </div>
+          <img :src="person.avatar" alt="" class="w-20 h-20" style="border-radius: 50%;">  
+          
+          <p>{{person.name}}</p>
+          <p>{{person.role}}</p>
+          <p>{{person.email}}</p>
+          <p>{{person.phone}}</p>
+          <p>{{person.dateOfBirth}}</p>
           
       </div>
     </div>
