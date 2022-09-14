1590d197a0958eded21cf890fb937646a9285cce
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2022-09-14 08:14:39 +0200

    min width staff

--- pages/staff.vue
+++ pages/staff.vue
@@ -3,7 +3,7 @@
     <div class="w-5/6 mx-auto bg-gray-800 px-24 rounded-xl" style="box-shadow: 0 0 48px 0 rgba(0,10,20,0.4);">
       <h1 class="text-gray-300 mt-20 pt-8 text-center mb-0" style="top:0;">Our Staff</h1>
       <div class="flex flex-wrap mx-auto py-12" style="justify-content: center; top:0;">
-        <div class="h-70 w-100 m-6 p-6 bg-gray-600 bg-cover text-gray-200 font-medium rounded-lg" style="box-shadow: 0 12px 24px 0 rgba(0,10,20,0.3);" v-for="person in people" :key="person.id" :style="{
+        <div class="h-70 w-100 m-6 p-6 bg-gray-600 bg-cover text-gray-200 font-medium rounded-lg" style="min-width: 300px; box-shadow: 0 12px 24px 0 rgba(0,10,20,0.3);" v-for="person in people" :key="person.id" :style="{
           textShadow: '0 0 6px rgba(0, 0, 30, 1)'
           }">
               <img :src="person.avatar" alt="" class="w-20 h-20 object-cover mb-2" style="border-radius: 50%;">
