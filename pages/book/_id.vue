7193768bef5fae5871b9c66819454e770ffff7f3
abbviksko <viktor.skold@abbgymnasiet.segit config --global user.email  viktor.skold@abbgymnasiet.se>
2022-09-14 16:18:29 +0200

    book id changes

--- pages/book/_id.vue
+++ pages/book/_id.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="min-h-screen bg-cover" style="background-image: url(https://static.vecteezy.com/system/resources/previews/003/421/363/original/abstract-modern-gradient-dark-blue-dynamic-stripes-background-free-vector.jpg);">
-        <h1 class="z-10 text-5xl justify-start text-white pt-24 w-6/12 justify-center " style="margin-left:20%;"> {{book.title}} </h1>
-        <div class="bg-gray-700 flex justify-start box-shadow w-7/12 rounded-xl" style="margin-left:20%;">
-            <img :src="book.img" class="w-4/12 rounded-l-xl">
-            <div class="p-4">
-                <h1>{{book.title}}</h1>
-                <h3>{{book.author}}</h3>
-
-            </div>
+      <h1 class="z-10 text-5xl justify-start text-white pt-24 w-6/12 justify-center " style="margin-left:20%;"> {{book.title}} </h1>
+      <div class="bg-gray-700 flex justify-start box-shadow w-7/12 rounded-xl" style="margin-left:20%;">
+        <img :src="book.img" class="w-4/12 rounded-l-xl">
+        <div class="p-4">
+          <h1 class="text-white">{{book.title}}</h1>
+          <h3 class="text-white">Author:&ensp; &emsp;    {{book.author}}</h3>
+          <h3 class="text-white">Genre: &ensp; &emsp;    {{book.genre}}</h3>
+          <h3 class="text-white">Language:&ensp;   {{book.lang}}</h3>
+          <h3 class="text-white">Copies left: {{book.copies}}</h3>
+          <button style="background: rgb(255,0,0);
+            background: linear-gradient(130deg, rgba(255,0,0,1) 0%, rgba(254,132,3,1) 16%, rgba(217,224,32,1) 32%, rgba(33,223,32,1) 50%, rgba(52,144,252,1) 75%, rgba(252,69,202,1) 100%);" class="h-4/12 w-4/12 p-2 box-shadow rounded mt-32 ml-80">Buy
+          </button>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 export default {
