b0ba5c1ee23a788fff51aec084c912cee7dc862c
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2022-09-15 12:18:24 +0200

    Search working!!

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,16 +2,16 @@
   <nav class="fixed w-full py-3 bg-gray-700 h-20 blur-xl z-40 font-medium flex justify-center" style="top:0;" >
     <div class="w-5/6 flex items-center justify-between">
       <div class="">
-        <form class="" style="right: 0;">   
-                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-                        <div class="relative">
-                            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                            </div>
-                            <input type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-transparent rounded-lg md:border md:border-transparent focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="search..." required="">
-                            <button type="submit" class="hidden md:flex text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-                        </div>
-                    </form>
+        <div style="right: 0;">   
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+            <div class="relative">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </div>
+                <input v-model="searchText" type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-transparent rounded-lg md:border md:border-transparent focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="search...">
+                <button @click="goToSearch" type="submit" class="hidden md:flex text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+        </div>
         <!-- <div class="h-full w-40" style="background-image: url(https://logodix.com/logo/48986.jpg);"></div> -->
       </div>
 
@@ -149,12 +149,17 @@ export default {
   data() {
     return {
       isOpen: false,
+      searchText: ""
     };
   },
   methods: {
     drawer() {
       this.isOpen = !this.isOpen;
     },
+    goToSearch() {
+      // alert('/search/'+this.searchText);
+      this.$router.push('/search/'+this.searchText)
+    }
   },
   watch: {
     isOpen: {
