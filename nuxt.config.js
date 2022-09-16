b9c2ac881e748901d44a48b4a9196b240f9c7cde
abbnoevah <92145566+abbnoevah@users.noreply.github.com>
2022-09-16 13:35:50 +0200

    FABULA

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Library - Speedy Kittens',
+    title: 'Fabula',
     htmlAttrs: {
       lang: 'en'
     },
